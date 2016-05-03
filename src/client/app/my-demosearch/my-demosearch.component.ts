6df67f0b0f74a360ae4cce5446bce9730e6dcafe
Andreas Hansson <andhan0305@gmail.com>
2016-05-03 17:57:46 +0200

    Fixed so the result of the search is shown in the console.

--- src/client/app/my-demosearch/my-demosearch.component.ts
+++ src/client/app/my-demosearch/my-demosearch.component.ts
@@ -7,19 +7,19 @@ import {Component, OnInit} from 'angular2/core';
   styleUrls: ['my-demosearch.component.css']
 })
 export class MyDemosearchComponent implements OnInit {
-  searchInput: {input: "hej"};
+  searchInput : {input: string} = {input: ""};
   constructor() {}
   
   ngOnInit() {
   }
   
   search() {
-    console.log('Inside search: ' + this.searchInput)
     var ref = new Firebase("https://sizzling-heat-4438.firebaseio.com/activities");
-    
+    var searchText = this.searchInput.input;
+
     ref.on("value", function (snapshot) {
       for (var item of snapshot.val()){
-        if (item.description.includes("Läx")){
+        if (item.description.includes(searchText)){
           console.log(item);
         }
       }
