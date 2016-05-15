a996a656bfd47db4aba58c2cb40562157f19d16b
Andreas Hansson <andhan0305@gmail.com>
2016-05-15 13:55:22 +0200

    Fixed so the user can create an account and login.

--- src/client/app/event-finder.component.ts
+++ src/client/app/event-finder.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, Inject} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {AngularFire} from 'angularfire2';
 import {MyMainComponent} from './my-main';
@@ -6,13 +6,19 @@ import {MyDemosearchComponent} from './my-demosearch';
 import {MyDetailviewComponent} from './my-detailview';
 import {MyShowDetailsviewComponent} from './my-show-detailsview';
 
+import {MyCreateAccountComponent} from './my-create-account/my-create-account.component';
+import {MyLoginComponent} from './my-login/my-login.component';
+import {FirebaseRef, FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2';
+import {CORE_DIRECTIVES} from 'angular2/common';
+import {DROPDOWN_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
+
 @Component({
   moduleId: __moduleName,
   selector: 'event-finder-app',
   providers: [ROUTER_PROVIDERS],
   templateUrl: 'event-finder.component.html',
   styleUrls: ['event-finder.component.css'],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES, DROPDOWN_DIRECTIVES],
   pipes: []
 })
 
@@ -22,9 +28,44 @@ import {MyShowDetailsviewComponent} from './my-show-detailsview';
   {path:'/my-demosearch', name: 'My-demosearch', component: MyDemosearchComponent},
   {path:'/my-detailview', name: 'My-detailview', component: MyDetailviewComponent},
   {path:'/my-show-detailsview', name: 'My-show-detailsview', component: MyShowDetailsviewComponent},
+  {path:'/create-account', name: 'CreateAccount', component: MyCreateAccountComponent},
+  {path:'/login', name: 'Login', component: MyLoginComponent},
   {path: '/*path', component: MyMainComponent}
 ])
 
 export class EventFinderApp {
-  constructor(private _af: AngularFire) {}
+  users: FirebaseListObservable<{}>;
+    constructor(public af : AngularFire, @Inject(FirebaseRef) public ref: Firebase) {}
+
+  
+    ngDoCheck() { 
+    if(this.users === undefined) {
+      try {
+        this.users =  this.af.database.list('/users/' + this.ref.getAuth().uid);
+        console.log("hej");
+      } catch(e) {
+        
+      }
+    }
+  }
+  
+  public disabled:boolean = false;
+  public status:{isopen:boolean} = {isopen: false};
+  public items:Array<string> = ['The first choice!',
+    'And another choice for you.', 'but wait! A third!'];
+
+  public toggled(open:boolean):void {
+    console.log('Dropdown is now: ', open);
+  }
+
+  public toggleDropdown($event:MouseEvent):void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
+  }
+  
+  public logout() {
+    this.users = undefined;
+    this.af.auth.logout();
+  }
 }
