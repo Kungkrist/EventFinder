c3da6e06acddba1a4e8e2dc474136377e941e327
Fredrik <fredriksvemark@live.se>
2016-05-26 15:29:13 +0200

    my-tile logic to service

--- src/client/app/my-tile/my-tile.component.ts
+++ src/client/app/my-tile/my-tile.component.ts
@@ -1,57 +1,28 @@
 import {Component, OnInit, Inject, Input} from 'angular2/core';
 import {Event, FullEvent} from '../IEvent';
 import {AngularFire, FirebaseListObservable, FirebaseRef} from 'angularfire2';
+import {MyEventsService} from '../my-events.service';
 
 @Component({
   moduleId: __moduleName,
   selector: 'my-tile',
   templateUrl: 'my-tile.component.html',
   styleUrls: ['my-tile.component.css'],
-  providers: []
+  providers: [MyEventsService]
 })
 export class MyTileComponent implements OnInit {
   @Input () eventId;
-  constructor(public af: AngularFire, @Inject(FirebaseRef) public ref: Firebase) {}
+  constructor(public das: MyEventsService) {}
   
-  event: FullEvent = {name: "",
-  date: "",
-  start_time: "",
-  stop_time: "",
-  info: "",
-  adress: "",
-  comments: [""],
-  price: "",
-  organiser: "",
-  phone: "",
-  email: "", 
-  uid: null,
-  imageURL: ""}
-
+  event = {};
+    
   ngOnInit() {
     
-     this.ref.child('/events/' + this.eventId).on("value", (a) => {
-       this.event = a.val();
-     });
-    // this.event = this.af.database.list('/events');
-  }
-  
-  checkPicture() {
-    /*if(this.event.imageURL !== '' && this.event.imageURL !== undefined) {
-      console.log(this.event.imageURL)
-      this.event.imageURL = 'http://i.imgur.com/jq4D5B6.png';
-      var picRef = new Firebase('');
-      picRef.once('value', (snapshot) => {
-        return snapshot.val();
-      }, err => {
-        console.log("error..." + err);
-        
-      });
-      return this.event.imageURL;
-    } else {
-      return this.event.imageURL;
-    }
+   this.das.getEvent(this.eventId).then(result => {
+     
+     this.event = result;
+   });
     
-   */
   }
 }
 
