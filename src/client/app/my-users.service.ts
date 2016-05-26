23cfe1f5b072af1aea1b495f557068cbc7b66476
Andreas Hansson <andhan0305@gmail.com>
2016-05-26 15:44:58 +0200

    Dixed so that the apicalls is handled on a users service.

--- src/client/app/my-users.service.ts
+++ src/client/app/my-users.service.ts
@@ -1,8 +1,25 @@
-import {Injectable} from 'angular2/core';
+import {Injectable, Inject} from 'angular2/core';
+import {AngularFire, FirebaseRef} from 'angularfire2/angularfire2'
 
 @Injectable()
 export class MyUsersService {
 
-  constructor() {}
-
+  constructor(public _af : AngularFire, @Inject(FirebaseRef) private _ref: Firebase) {}
+  
+  createAccount(email: string, password: string){    
+    return new Promise(resolve => {        
+      this._ref.createUser({
+      email: email,
+      password: password
+      }, 
+      (error, userData) => {
+        let result = {"error": error, "userData": userData}
+        resolve(result);
+      });
+    });         
+  }
+  
+  addUser(userId: String, data : any) {
+    this._af.database.object("/users/" + userId).set(data);    
+  }
 }
